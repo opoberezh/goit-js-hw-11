70b06f522c4bc3ea47afb3ed17df527708f6448b
opoberezh <opoberezh.2015@gmail.com>
2023-07-10 20:43:23 +0200

    completed

--- src/get_api.js
+++ src/get_api.js
@@ -1,72 +1,102 @@
-import axios from 'axios';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import axios from 'axios';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-let BASE_URL = 'https://pixabay.com/api/';
-let API_KEY = '38119446-41822b71524f1b118d79216dc';
-let searchQuery = '';
-let currentPage = 1;
-let total = null;
-let firstSearch = true;
+// let BASE_URL = 'https://pixabay.com/api/';
+// let API_KEY = '38119446-41822b71524f1b118d79216dc';
+// let searchQuery = '';
+// let currentPage = 1;
+// let total = null;
+// let firstSearch = true;
 
-// restart searching
-export function updateFirstSearch(state) {
-  firstSearch = state;
+// // restart searching
+// export function updateFirstSearch(state) {
+//   firstSearch = state;
  
-}
+// }
 
-// to go to next page
-export function nextPage() {
-  currentPage = currentPage + 1;
-  return getImages();
-}
+// // to go to next page
+// export function nextPage() {
+//   currentPage = currentPage + 1;
+//   return getImages();
+// }
 
-// reset nomber of page
-export function resetPage() {
-  currentPage = 1;
-}
+// // reset nomber of page
+// export function resetPage() {
+//   currentPage = 1;
+// }
 
-// API request, img response
-export async function getImages() {
-  let params = {
-    key: API_KEY,
-    q: searchQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    page: currentPage,
-    per_page: 40,
-  };
+// // API request, img response
+// export async function getImages() {
+//   let params = {
+//     key: API_KEY,
+//     q: searchQuery,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: 'true',
+//     page: currentPage,
+//     per_page: 40,
+//   };
 
+//   try {
+//     let response = await axios.get(BASE_URL, { params: params });
+
+//     total = response.data.total;
+
+//     if (firstSearch) {
+//       updateFirstSearch(false);
+//     }
+
+//     let imagesData = response.data.hits.map(hit => {
+//       return {
+//         webformatURL: hit.webformatURL.replace('_640', '_340'),
+//         largeImageURL: hit.largeImageURL,
+//         tags: hit.tags,
+//         views: hit.views,
+//         comments: hit.comments,
+//         downloads: hit.downloads,
+//         likes: hit.likes,
+//       };
+//     });
+
+//     return imagesData;
+//   } catch (error) {
+//     console.log(error)
+//     Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+//   }
+// }
+
+// // new searchQuery request
+// export function setSearchQuery(newQuery) {
+//   searchQuery = newQuery;
+// }
+
+
+
+
+import axios from 'axios';
+import { nameImages, currentPage } from './index';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38119446-41822b71524f1b118d79216dc';
+
+async function getImages() {
   try {
-    let response = await axios.get(BASE_URL, { params: params });
-
-    total = response.data.total;
-
-    if (firstSearch) {
-      updateFirstSearch(false);
-    }
-
-    let imagesData = response.data.hits.map(hit => {
-      return {
-        webformatURL: hit.webformatURL.replace('_640', '_340'),
-        largeImageURL: hit.largeImageURL,
-        tags: hit.tags,
-        views: hit.views,
-        comments: hit.comments,
-        downloads: hit.downloads,
-        likes: hit.likes,
-      };
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: nameImages,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: currentPage,
+      },
     });
 
-    return imagesData;
+    return response;
   } catch (error) {
-    console.log(error)
-    Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    console.error(error);
   }
 }
-
-// new searchQuery request
-export function setSearchQuery(newQuery) {
-  searchQuery = newQuery;
-}
+export { getImages };
--- src/markup.js
+++ src/markup.js
@@ -1,37 +1,80 @@
-export function createMarkup(data) {
-    return data
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          views,
-          comments,
-          downloads,
-          likes,
-        }) =>
-          `<div class="photo-card">
-    <a href="${largeImageURL}">
-      <img class="image" src="${webformatURL}" alt="${tags}" width = 460 height = 340 loading="lazy"/>
-      <div class="info">
+// export function createMarkup(data) {
+//     return data
+//       .map(
+//         ({
+//           webformatURL,
+//           largeImageURL,
+//           tags,
+//           views,
+//           comments,
+//           downloads,
+//           likes,
+//         }) =>
+//           `<div class="photo-card">
+//     <a href="${largeImageURL}">
+//       <img class="image" src="${webformatURL}" alt="${tags}" width = 460 height = 340 loading="lazy"/>
+//       <div class="info">
     
-       <p class="info-item">
-            <i class="fa-regular fa-heart"></i> ${likes}
-       </p>
+//        <p class="info-item">
+//             <i class="fa-regular fa-heart"></i> ${likes}
+//        </p>
        
-        <p class="info-item">
-            <i class="fa-solid fa-comment"></i> ${comments}
-        </p>
-        <p class="info-item">
-             <i class="fa-solid fa-download"></i> ${downloads}
-        </p>
+//         <p class="info-item">
+//             <i class="fa-solid fa-comment"></i> ${comments}
+//         </p>
+//         <p class="info-item">
+//              <i class="fa-solid fa-download"></i> ${downloads}
+//         </p>
 
-        <p class="info-item">
-        <b>Views: ${views}</b>
-      </p>
-      </div>
-    </a>
-  </div>`
-      )
-      .join('');
-  }
+//         <p class="info-item">
+//         <b>Views: ${views}</b>
+//       </p>
+//       </div>
+//     </a>
+//   </div>`
+//       )
+//       .join('');
+//   }
+
+
+
+
+function markupGellery(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<div class="photo-card">
+            
+            <div class="thumb-img">
+                <a class="gallery-link" href=${largeImageURL}>
+                    <img class="gallery-image" src=${webformatURL} alt="${tags}" loading="lazy"/>
+                </a>
+            </div>
+            
+            <div class="info">
+                <p class="info-item">
+                <b>Likes</b>${likes}
+                </p>
+                <p class="info-item">
+                <b>Views</b>${views}
+                </p>
+                <p class="info-item">
+                <b>Comments</b>${comments}
+                </p>
+                <p class="info-item">
+                <b>Downloads</b>${downloads}
+                </p>
+            </div>
+        </div>`
+    )
+    .join('');
+}
+export { markupGellery };
